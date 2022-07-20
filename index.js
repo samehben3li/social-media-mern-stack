6d718e3f7e0e7b2b513b41fe8d6325194e9730af
sameh <samehbenali69@gmail.com>
2022-07-20 20:18:33 +0100

    add upload end point

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv")
 const helmet = require("helmet")
 const morgan = require("morgan")
+const path = require("path")
+const multer = require("multer")
 
 const userRoute = require("./routes/users")
 const authRoute = require("./routes/auth")
@@ -10,20 +12,35 @@ const postRoute = require("./routes/posts")
 
 const app = express()
 dotenv.config()
-mongoose 
- .connect(process.env.MONGO_URI, {
-        useNewUrlParser: true})   
- .then(() => console.log("Database connected!"))
- .catch(err => console.log(err));
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true
+    })
+    .then(() => console.log("Database connected!"))
+    .catch(err => console.log(err));
 
 app.use(express.json())
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
 app.use(helmet())
 app.use(morgan("common"))
 
-app.use("/api/users",userRoute)
-app.use("/api/auth",authRoute)
-app.use("/api/posts",postRoute)
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads")
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name)
+    }
+})
+const upload = multer({ storage: storage })
 
-app.listen(8800,()=>{
+app.use("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("file has been upload ...")
+})
+app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute)
+app.use("/api/posts", postRoute)
+
+app.listen(8800, () => {
     console.log("Backend server is running!")
 })
